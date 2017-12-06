498f80d563632812c6257315b113c2e383bb25f2
Priyanka Negi <prinegi100@gmail.com>
2017-12-06 15:28:04 +0530

    -modification

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-before(() => {
+before((done) => {
   
   mongoose.connect('mongodb://localhost/users_test');
   mongoose.connection
-  .once('open', () => console.log('Good to go!'))
+  .once('open', () =>{ console.log('Good to go!'); done();})
   .on('error', () => console.warn('Warning', error));
 })
 beforeEach((done) => {
