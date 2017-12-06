e0591728fe4890e39a409078b9b8baafe6e21c8d
Priyanka Negi <prinegi100@gmail.com>
2017-12-06 16:13:41 +0530

    completed resd_test.js

---
+++ test/read_test.js
@@ -0,0 +1,23 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Reading users out of the database', () => {
+
+  let joe;
+
+  beforeEach((done) => {
+    joe = new User({ name: 'Joe'});
+    joe.save()
+      .then(() => {
+        done()
+      });
+  })
+
+  it('finds all the users with tha name of joe ', () => {
+    User.find({ name: 'Joe' })
+    .then((users) => {
+      console.log(users);
+      done();
+    })
+  })
+})
