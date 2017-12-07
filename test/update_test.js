d1c49ead195c200f19e6e675a80614991705082b
Priyanka Negi <prinegi100@gmail.com>
2017-12-07 15:06:00 +0530

    sent promise as a args to assertName()

--- test/update_test.js
+++ test/update_test.js
@@ -11,14 +11,18 @@ describe('Updating records', () => {
     })
   });
 
+function assertName(operationOnPromise, done){
+  operationOnPromise
+  .then(() => User.find({}))
+  .then((users) => {
+    assert(users.length === 1);
+    assert(users[0].name === 'Alex');
+    done();
+  });
+
+}
   it('instance type using set and save', (done) => {
     joe.set('name', 'Alex');
-    joe.save()
-    .then(() => User.find({}))
-    .then((users) => {
-      assert(users.length === 1);
-      assert(users[0].name === 'Alex');
-      done();
-    });
+    assertName(joe.save(), done);
   })
 })
